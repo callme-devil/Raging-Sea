734818a8f8cd5e32b70277534749733500dbcd53
callme-devil <pransermi@gmail.com>
2023-01-13 21:40:13 +0330

    feat: Added wave sea shader

--- src/script.js
+++ src/script.js
@@ -26,8 +26,18 @@ const waterGeometry = new THREE.PlaneGeometry(2, 2, 128, 128)
 // Material
 const waterMaterial = new THREE.ShaderMaterial({
     fragmentShader: waterFragmentShader,
-    vertexShader: waterVertexSahder
+    vertexShader: waterVertexSahder,
+    uniforms: {
+        uBigWavesElevation: {value: 0.2},
+        uBigWavesFrequency: {value: new THREE.Vector2(4 , 1.5)}
+    }
 })
+
+// Debug
+
+gui.add(waterMaterial.uniforms.uBigWavesElevation , 'value').min(0).max(1).step(0.001).name('uBigWavesElevation')
+gui.add(waterMaterial.uniforms.uBigWavesFrequency.value , 'x').min(0).max(10).step(0.001).name('uBigWavesFrequency.X')
+gui.add(waterMaterial.uniforms.uBigWavesFrequency.value , 'y').min(0).max(10).step(0.001).name('uBigWavesFrequency.Y')
 
 // Mesh
 const water = new THREE.Mesh(waterGeometry, waterMaterial)
