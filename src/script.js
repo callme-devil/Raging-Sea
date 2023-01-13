67c3ce0de4c42abc7687bdc13a5371e40627bc58
callme-devil <pransermi@gmail.com>
2023-01-13 21:52:29 +0330

    feat: Animated wave

--- src/script.js
+++ src/script.js
@@ -10,6 +10,7 @@ import waterFragmentShader from './shaders/water/fragment.glsl'
  */
 // Debug
 const gui = new dat.GUI({ width: 340 })
+const debugObject = {}
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -23,13 +24,22 @@ const scene = new THREE.Scene()
 // Geometry
 const waterGeometry = new THREE.PlaneGeometry(2, 2, 128, 128)
 
+// Debug Color
+debugObject.depthColor = '#0000ff'
+debugObject.surfaceColor = '#8888ff'
+
 // Material
 const waterMaterial = new THREE.ShaderMaterial({
     fragmentShader: waterFragmentShader,
     vertexShader: waterVertexSahder,
     uniforms: {
+        uTime: {value: 0},
         uBigWavesElevation: {value: 0.2},
-        uBigWavesFrequency: {value: new THREE.Vector2(4 , 1.5)}
+        uBigWavesFrequency: {value: new THREE.Vector2(4 , 1.5)},
+        uBigWavesSpeed: {value : 0.75},
+
+        uDepthColor: {value : new THREE.Color(debugObject.depthColor)},
+        uSurfaceColor: {value : new THREE.Color(debugObject.surfaceColor)}
     }
 })
 
@@ -38,6 +48,12 @@ const waterMaterial = new THREE.ShaderMaterial({
 gui.add(waterMaterial.uniforms.uBigWavesElevation , 'value').min(0).max(1).step(0.001).name('uBigWavesElevation')
 gui.add(waterMaterial.uniforms.uBigWavesFrequency.value , 'x').min(0).max(10).step(0.001).name('uBigWavesFrequency.X')
 gui.add(waterMaterial.uniforms.uBigWavesFrequency.value , 'y').min(0).max(10).step(0.001).name('uBigWavesFrequency.Y')
+gui.add(waterMaterial.uniforms.uBigWavesSpeed , 'value').min(0).max(10).step(0.001).name('uBigWavesSpeed')
+gui.addColor(debugObject , 'depthColor').onChange(() => {waterMaterial.uniforms.uDepthColor.value.set(debugObject.depthColor)})
+.name('depthColor')
+gui.addColor(debugObject , 'surfaceColor').onChange(() => {waterMaterial.uniforms.uDepthColor.value.set(debugObject.surfaceColor)})
+.name('surfaceColor')
+
 
 // Mesh
 const water = new THREE.Mesh(waterGeometry, waterMaterial)
@@ -96,6 +112,9 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    // Update water
+    waterMaterial.uniforms.uTime.value = elapsedTime
 
     // Update controls
     controls.update()
--- src/shaders/water/vertex.glsl
+++ src/shaders/water/vertex.glsl
@@ -1,13 +1,15 @@
 uniform float uBigWavesElevation;
 uniform vec2 uBigWavesFrequency;
+uniform float uTime;
+uniform float uBigWavesSpeed;
 
 void main(){
 
     vec4 modelPosition = modelMatrix * vec4(position , 1.0);
 
     float elevation = 
-        sin(modelPosition.x * uBigWavesFrequency.x) * 
-        sin(modelPosition.z * uBigWavesFrequency.y) * 
+        sin(modelPosition.x * uBigWavesFrequency.x + uTime * uBigWavesSpeed) * 
+        sin(modelPosition.z * uBigWavesFrequency.y + uTime * uBigWavesSpeed) * 
         uBigWavesElevation;
 
     modelPosition.y += elevation;
